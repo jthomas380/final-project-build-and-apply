690a33355c147ad3420933249078e4ab4dfb5d68
Jason E. Thomas <jethomas1130@gmail.com>
2026-08-07 12:16:30 -0400

    completed text for section one left column and wired buttons

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const navButton = document.querySelector(".nav__button");
+const menuToggle = document.querySelector(".nav__menu-toggle");
+const navLinks = document.querySelector(".nav__links");
 
-navButton.addEventListener("click", function () {
-  window.location.href = "https://www.google.com";
-});
+if (navButton) {
+  navButton.addEventListener("click", function () {
+    window.location.href = "https://www.google.com";
+  });
+}
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+
+  navLinks.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("open");
+    });
+  });
+}
+
+// First section //
+
+const fsecButtonRight = document.querySelector(".fsec__button--right");
+const fsecButtonLeft = document.querySelector(".fsec__button--left");
+
+if (fsecButtonRight) {
+  fsecButtonRight.addEventListener("click", function () {
+    window.location.href = "https://www.google.com";
+  });
+}
+
+if (fsecButtonLeft) {
+  fsecButtonLeft.addEventListener("click", function () {
+    window.location.href = "https://www.google.com";
+  });
+}
